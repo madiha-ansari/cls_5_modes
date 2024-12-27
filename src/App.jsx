eda5fe2329f75e033ce00206d8371d61d16048b3
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-12-27 17:11:12 +0500

    hm

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import { GoSun } from "react-icons/go";
+import { FaMoon } from "react-icons/fa";
+
+function App() {
+  const [isLit, setLit] = useState(true);
+  const changeTheme = () => {
+    setLit(!isLit);
+    console.log("Theme changed to:", isLit ? "Dark" : "Light");
+  };
+
+  return (
+    <div className={`main ${isLit ? "light" : "dark"}`}>
+      <div className="theme-toggle">
+        <label className="toggle-label">
+          <input
+            onClick={changeTheme}
+            className="hidden"
+            type="checkbox"
+          />
+          <span className="icon">
+            {isLit ? <GoSun size={30} /> : <FaMoon size={30} />}
+          </span>
+        </label>
+        <p className="status-text">
+          {isLit ? "Light Mode" : "Dark Mode"}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
